b44fdbf047e776bda41ccd75c369e3a478e479d0
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2020-01-01 21:08:57 -0400

    Pausa para comer.. no funciona el listView no actualiza datos

--- app/src/main/java/com/example/arduinobluetoothcomunicacion/MonitorSerial.kt
+++ app/src/main/java/com/example/arduinobluetoothcomunicacion/MonitorSerial.kt
@@ -2,11 +2,9 @@ package com.example.arduinobluetoothcomunicacion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import java.io.IOException
+import java.util.ArrayList
 
 class MonitorSerial : AppCompatActivity() {
 
@@ -14,14 +12,15 @@ class MonitorSerial : AppCompatActivity() {
     lateinit var bt_enviar : Button
     lateinit var dato : String
     lateinit var txtprueba : TextView
-    companion object {
-        lateinit var MyConectionBt : Thread
-    }
+    lateinit var listView : ListView
+    var mutableList: MutableList<String> = mutableListOf()
+    lateinit var adp : ArrayAdapter<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_monitor_serial)
 
+        listView = findViewById(R.id.id_lista)
         texto = findViewById(R.id.id_entrada)
         txtprueba = findViewById(R.id.id_prueba)
         bt_enviar = findViewById(R.id.id_bt_enviar)
@@ -40,10 +39,15 @@ class MonitorSerial : AppCompatActivity() {
         if (MainActivity.m_bluetooth_Socket != null) {
             try{
                 MainActivity.m_bluetooth_Socket!!.outputStream.write(input.toByteArray())
+                mutableList.add(input)
+                adp = ArrayAdapter(this, android.R.layout.simple_list_item_1,mutableList)
+                listView.adapter = adp
                 Toast.makeText(this, "Dato enviado: "+input,Toast.LENGTH_SHORT).show()
             } catch(e: IOException) {
                 e.printStackTrace()
             }
         }
     }
+
+
 }
